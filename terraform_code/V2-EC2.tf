39e36d659831bb081cadce3ef758c441771883f1
Danyal Khanzada <78877585+DanyalKhanzada@users.noreply.github.com>
2024-07-12 18:48:13 -0400

    deleting previous code

--- terraform_code/V2-EC2.tf
+++ terraform_code/V2-EC2.tf
@@ -1,39 +1,3 @@
-/*provider "aws" {
-    region  = "us-east-1"
-}
-
- resource "aws_instance" "demo-server" {
-  ami           = "ami-06c68f701d8090592"
-  instance_type = "t2.micro"
-  key_name = "dpp"
-  security_groups = ["demo-sg"]
- }
-
-resource "aws_security_group" "demo-sg" {
-  name        = "demo-sg"
-  description = "SSh Access"
-  vpc_id      = aws_vpc.main.id
-
-  tags = {
-    Name = "ssh-port"
-  }
-}
-
-
-resource "aws_vpc_security_group_ingress_rule" "allow_ssh" {
-  security_group_id = demo-sg
-  description       = "SSH Access"
-  from_port         = 22
-  ip_protocol       = "tcp"
-  to_port           = 22
-  cidr_ipv4         = ["0.0.0.0/0"]
-}
-
-resource "aws_vpc_security_group_egress_rule" "allow_all_traffic_ipv6" {
-  security_group_id = demo-sg
-  cidr_ipv6         = "::/0"
-  ip_protocol       = "-1" # semantically equivalent to all ports
-}*/
 provider "aws" {
     region  = "us-east-1"
 }
