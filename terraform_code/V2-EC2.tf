6484fa39b287b65c01f32228cff803d5ae38672b
Danyal Khanzada <78877585+DanyalKhanzada@users.noreply.github.com>
2024-07-12 19:13:33 -0400

    adding subnets and route tables

--- terraform_code/V2-EC2.tf
+++ terraform_code/V2-EC2.tf
@@ -6,12 +6,14 @@ resource "aws_instance" "demo-server" {
   ami           = "ami-06c68f701d8090592"
   instance_type = "t2.micro"
   key_name      = "dpp"
-  vpc_security_group_ids = [aws_security_group.demo-sg.id]
+  security_groups = [ "demo-sg" ]
+  subnet_id = aws_subnet.dpp-public_subnet_01.id
 }
 
 resource "aws_security_group" "demo-sg" {
   name        = "demo-sg"
   description = "SSH Access"
+  vpc_id = aws_vpc.dpp-vpc
 
   ingress {
     description = "SSH access"
@@ -32,4 +34,65 @@ resource "aws_security_group" "demo-sg" {
   tags = {
     Name = "ssh-port"
   }
+}
+
+//VPC
+  resource "aws_vpc" "dpp-vpc" {
+    cidr_block = "10.1.0.0/16"
+    tags = {
+    Name = "dpp-vpc"
+    }
+}
+
+
+//Create a Subnet 
+resource "aws_subnet" "dpp-public_subnet_01" {
+    vpc_id = aws_vpc.dpp-vpc.id
+    cidr_block = "10.1.1.0/24"
+    map_public_ip_on_launch = "true"
+    availability_zone = "us-east-1a"
+    tags = {
+      Name = "dpp-public_subnet_01"
+    }
+}
+
+resource "aws_subnet" "dpw-public_subnet_02" {
+    vpc_id = aws_vpc.dpp-vpc.id
+    cidr_block = "10.1.2.0/24"
+    map_public_ip_on_launch = "true"
+    availability_zone = "us-east-1b"
+    tags = {
+      Name = "dpp-public_subnet_02"
+    }
+}
+
+//Creating an Internet Gateway 
+resource "aws_internet_gateway" "dpw-igw" {
+    vpc_id = aws_vpc.dpp-vpc.id
+    tags = {
+      Name = "dpp-igw"
+    }
+}
+
+// Create a route table 
+resource "aws_route_table" "dpp-public-rt" {
+    vpc_id = aws_vpc.dpp-vpc.id
+    route {
+        cidr_block = "0.0.0.0/0"
+        gateway_id = aws_internet_gateway.dpp-igw.id
+    }
+    tags = {
+      Name = "dpp-public-rt"
+    }
+}
+
+resource "aws_route_table_association" "dpp-rta-public-subnet-01" {
+    subnet_id = aws_subnet.dpp-public_subnet_01.id
+    route_table_id = aws_route_table.dpp-public-rt.id
+}
+
+
+resource "aws_route_table_association" "dpp-rta-public-subnet-02" {
+    subnet_id = aws_subnet.dpp-public_subnet_02.id 
+    route_table_id = aws_route_table.dpp-public-rt.id
 }
