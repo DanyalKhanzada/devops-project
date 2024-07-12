1384b3024e1a63932abef1ebccd79ca3c5558ef2
Danyal Khanzada <78877585+DanyalKhanzada@users.noreply.github.com>
2024-07-12 18:47:28 -0400

    updated security group

--- terraform_code/V2-EC2.tf
+++ terraform_code/V2-EC2.tf
@@ -1,4 +1,4 @@
-provider "aws" {
+/*provider "aws" {
     region  = "us-east-1"
 }
 
@@ -21,7 +21,7 @@ resource "aws_security_group" "demo-sg" {
 
 
 resource "aws_vpc_security_group_ingress_rule" "allow_ssh" {
-  security_group_id = aws_security_group.main.id
+  security_group_id = demo-sg
   description       = "SSH Access"
   from_port         = 22
   ip_protocol       = "tcp"
@@ -30,7 +30,42 @@ resource "aws_vpc_security_group_ingress_rule" "allow_ssh" {
 }
 
 resource "aws_vpc_security_group_egress_rule" "allow_all_traffic_ipv6" {
-  security_group_id = aws_security_group.allow_tls.id
+  security_group_id = demo-sg
   cidr_ipv6         = "::/0"
   ip_protocol       = "-1" # semantically equivalent to all ports
+}*/
+provider "aws" {
+    region  = "us-east-1"
+}
+
+resource "aws_instance" "demo-server" {
+  ami           = "ami-06c68f701d8090592"
+  instance_type = "t2.micro"
+  key_name      = "dpp"
+  vpc_security_group_ids = [aws_security_group.demo-sg.id]
+}
+
+resource "aws_security_group" "demo-sg" {
+  name        = "demo-sg"
+  description = "SSH Access"
+
+  ingress {
+    description = "SSH access"
+    from_port   = 22
+    to_port     = 22
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  egress {
+    from_port        = 0
+    to_port          = 0
+    protocol         = "-1"
+    cidr_blocks      = ["0.0.0.0/0"]
+    ipv6_cidr_blocks = ["::/0"]
+  }
+
+  tags = {
+    Name = "ssh-port"
+  }
 }
